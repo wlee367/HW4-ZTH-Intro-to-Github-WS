584f13396ec2333d449e76932b3cf63df07c3392
Won Ryul Lee <wlee367@uwo.ca>
2017-10-09 17:34:34 -0400

    adding example python script to mess up during presentations

---
+++ some_cool_things.py
@@ -0,0 +1,39 @@
+'''
+1) Guess My Number:
+Overview:
+The computer randomly generates a number. The user inputs a number, and the computer will tell you if you are too high, 
+or too low. Then you will get to keep guessing until you guess the number.
+'''
+
+# main
+from random import randint
+
+guessesNum = 0
+number = randint(0,9)
+
+print('Welcome Stranger, I am thinking of a number between 0 and 9, inclusive. You have 5 chances' )
+while guessesNum < 5:
+	print('Take a guess')
+	guess = input()
+	guess = int(guess)
+
+	guessesNum = guessesNum + 1
+
+	if guess < number:
+		print('Incorrect, your guess is too low')
+	if guess > number:
+		print('Incorrect, your Guess is too high')
+
+	if guess == number:
+		break
+
+if guess == number:
+	print('Good job, Stranger. You guessed my number in {} times'.format(guessesNum))
+
+if guess!=number: 
+
+	print('Nah fam, I was thinking of {}'.format(number))
+
+for i in range(0,8):
+	print('\n')
+print('That was some cool things eh')
